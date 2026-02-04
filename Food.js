4c42231f48e1bb65a0a4a26926e96081cbdabe4b
Sakura <owenkaufman94@gmail.com>
2026-02-04 09:57:43 -0500

    Added FOOD!!!!!
    delectable

--- Food.js
+++ Food.js
@@ -1,9 +1,16 @@
 function Food(type, name, value) {
-    let self = {
-        Type:  type  || "None",
-        Name:  name  || "Food", //pretend that this is readable
-        Value: value || 0,
-    }
+    //i do not like JS objects. i have no idea if this will actually run cuz its so confusing
+    this.Type  = type  || "None"
+    this.Name  = name  || "NewFood" //quick pretend this is readable
+    this.Value = value || 0
+    this.Eaten = false
 
-    return self
+    //Makes the pet eat the food, returns the food's value or 0 if it's already eaten
+    this.Eat = function() { //oh god please help me this better not all be in here
+        if (this.Eaten) {
+            return 0
+        }
+        this.Eaten = true
+        return this.Value
+    }
 }
