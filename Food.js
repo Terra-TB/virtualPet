33f3be0b3cbe8c4d015848cef35375c2c82baa86
Sakura <owenkaufman94@gmail.com>
2026-02-04 10:12:42 -0500

    Food changes
    ill never stop updating this

--- Food.js
+++ Food.js
@@ -1,8 +1,18 @@
+/*
+    Heres what all these variables do exactly:
+    
+    Type:  The type of food (Food, Drink, etc)
+    Name:  The name of the food ("Pizza" for example)
+    Value: How much hunger the pet restores from eating the food
+    Eaten: Tells if the food has already been consumed (Usually changed by the Eat method)
+
+*/
+
 function Food(type, name, value) {
     //i do not like JS objects. i have no idea if this will actually run cuz its so confusing
     this.Type  = type  || "None"
     this.Name  = name  || "NewFood" //quick pretend this is readable
-    this.Value = value || 0
+    this.Value = value || 0 
     this.Eaten = false
 
     //Makes the pet eat the food, returns the food's value or 0 if it's already eaten
@@ -13,4 +23,17 @@ function Food(type, name, value) {
         this.Eaten = true
         return this.Value
     }
+
+    this.toString = function() {
+        let output = ""
+
+        output += this.Name + ": \n"
+        output += "Type: " + this.Type + ", \n"
+        output += "Value: " + this.Value + ", \n"
+        output += "Already Eaten: " + (!this.Eaten && "No" || "Yes") + ", \n"
+        
+        return output
+    }
+  
+    return this //okay its starting to make sense (pray that this is actually how you do objects in this god forsaken language)
 }
